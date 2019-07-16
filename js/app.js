599f23895dd309d01c4292c047190763e88d588b
James Dansie <jdansie8336@gmail.com>
2019-07-16 15:13:36 -0700

    functional table! :D

--- js/app.js
+++ js/app.js
@@ -21,7 +21,10 @@
 //  add cookie/hr to locSales
 
 var locationsArr = [];
-// Constructor
+var tableEl = document.getElementById('table');
+var time = ['6 AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
+//*****************Constructor********************
+
 function Location(name, customerMinPerHour, customerMaxPerHour, cookieAverage){
   this.name = name;
   this.customerMinPerHourNum = customerMinPerHour;
@@ -30,25 +33,34 @@ function Location(name, customerMinPerHour, customerMaxPerHour, cookieAverage){
   this.cookieTotalNum = 0;
   this.locSalesArr = [];
   this.ulEl = document.getElementById(this.name);
-  this.time = ['6 AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
   locationsArr.push(this);
 }
 
-// Prototype Stuff
+//**************** Prototype Stuff*****************
+//random number generator
 Location.prototype.getRandomInt = function(min, max) {
   return Math.round(Math.random() * (max-min+1)) + min;
 };
+
+//make a cookie count
 Location.prototype.cookieCount = function(){
-  for(var index = 0; index < this.time.length; index++) {
+  for(var index = 0; index < time.length; index++) {
+    //get random number
     var customersPerHourNum = this.getRandomInt(this.customerMinPerHourNum, this.customerMaxPerHourNum);
+
+    //cookies = customers * cookie average
     var cookiesPerHourNum = Math.round(customersPerHourNum * this.cookieAverageNum);
+
+    //assigning values to arrays
     this.cookieTotalNum += cookiesPerHourNum;
     this.locSalesArr.push(cookiesPerHourNum);
   }
 };
-Location.prototype.render = function(){
+
+//Render method for lists
+Location.prototype.renderList = function(){
   this.cookieCount();
-  for(var index = 0; index < this.time.length; index++){
+  for(var index = 0; index < time.length; index++){
     //got this layout from today's notes
     // render function
     // three things
@@ -56,7 +68,7 @@ Location.prototype.render = function(){
     var liEl = document.createElement('li');
 
     // 2. give it content - name
-    liEl.textContent = `${this.time[index]}: ${this.locSalesArr[index]} cookies`;
+    liEl.textContent = `${time[index]}: ${this.locSalesArr[index]} cookies`;
 
     // 3. append it to the DOM
     this.ulEl.appendChild(liEl);
@@ -71,12 +83,56 @@ Location.prototype.render = function(){
 
 
 };
+
+//Render method for tables
+Location.prototype.renderTable = function(){
+  //declaring elements
+  var trEl = document.createElement('tr');
+  tableEl.appendChild(trEl);
+
+  //Writing the first element with the name
+  var thEl = document.createElement('th');
+  thEl.textContent = this.name;
+  trEl.appendChild(thEl);
+
+  //looping through the sale array
+  for(var index = 0; index < time.length; index++){
+    thEl = document.createElement('th');
+    thEl.textContent = this.locSalesArr[index];
+    trEl.appendChild(thEl);
+  }
+};
+
+//*************Helper Functions */
+//Makes the Header
+function makeHeader(){
+  //declaring elements
+  var trEl = document.createElement('tr');
+  tableEl.appendChild(trEl);
+
+  //Writing the first element with 'location'
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location';
+  trEl.appendChild(thEl);
+
+  //Writing the time stample for the rest of the header
+  for(var i = 0; i < time.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = time[i];
+    trEl.appendChild(thEl);
+  }
+}
+
 new Location('pike', 23, 65, 6.3);
 new Location('sea', 3, 24, 1.2);
 new Location('center', 11, 38, 3.7);
 new Location('capHill', 20, 38, 2.3);
 new Location('alki', 2, 16, 4.6);
 
+
+makeHeader();
+
 for(var index = 0; index < locationsArr.length; index++){
-  locationsArr[index].render();
+  locationsArr[index].renderList();
+  locationsArr[index].renderTable();
 }
